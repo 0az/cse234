ddafd1394bf314be827c541651e95d9f88a56c73
Arunav Gupta <centaurus.ab@gmail.com>
2021-12-08 19:29:46 -0800

    feat: abstract Model and add LSTM

--- cerebro_ts/models.py
+++ cerebro_ts/models.py
@@ -33,11 +33,11 @@ from .prepare import prep_data_spark
 LOGGER = get_logger(__name__)
 
 
-class MLP:
+class Model:
     DATAPREP_PARAMS = {"window_size", "sample_strategy"}
     DEFAULTS = {
         "window_size": [1],
-        "sample_strategy": ['mean'],
+        "sample_strategy": ["mean"],
         "lr": [0.001],
         "momentum": [0.9],
         "batch_size": [8],
@@ -67,18 +67,20 @@ class MLP:
         self.dataprep_params = {}
         self.params = {}
         # ensure defaults are in dict
-        for d in MLP.DEFAULTS:
+        for d in Model.DEFAULTS:
             if d not in params:
-                params[d] = MLP.DEFAULTS[d]
+                params[d] = Model.DEFAULTS[d]
         # separate dataprep params from cerebro params
         for p in params:
-            if p in MLP.DATAPREP_PARAMS:
+            if p in Model.DATAPREP_PARAMS:
                 self.dataprep_params[p] = params[p]
                 # if p == 'window_size':
                 #     # do the +1 because we pass in the window columns +  the original column
                 #     self.params['input_dim'] = hp_choice(list(map(lambda x: x + 1, params[p])))
             else:
-                self.params[p] = hp_choice(params[p])
+                self.params[p] = (
+                    hp_choice(params[p]) if type(params[p]) == list else params[p]
+                )
 
         valid_cols = set(dataset.columns).intersection(
             set([id_col, time_col, value_col, label_col])
@@ -86,7 +88,7 @@ class MLP:
         self.dataset = dataset.select(*valid_cols)
         self.spark = spark
         self.cache_prepped_data = cache_prepped_data
-        self.n_workers = n_workers
+        self.num_workers = num_workers
 
         self.input_dim = None
 
@@ -97,13 +99,15 @@ class MLP:
         self.label_col = label_col
 
         self.best_model = None
-        self.best_acc = float('-inf')
+        self.best_acc = float("-inf")
         self.best_w = None
 
-        # Create cerebro primitives
+        # Create cerebro basics
         self.backend = SparkBackend(
-            spark_context=self.spark.sparkContext, num_workers=self.n_workers
+            spark_context=self.spark.sparkContext, num_workers=self.num_workers
         )
+        self.store = LocalStore(prefix_path=os.path.join(os.getcwd(), "cerebro_store"))
+
         if store_type == 'local':
             self.store = LocalStore(prefix_path=store_path)
         elif store_type == 'hdfs':
@@ -117,6 +121,98 @@ class MLP:
         if self.cache_prepped_data:
             # compute the dataframes now and cache them
             pass
+
+    def estimator_gen_fn(self, params):
+        raise NotImplementedError("Model function not implemented.")
+
+    def train(self, epochs=10):
+        """Trains model on pre-loaded data."""
+        if not self.cache_prepped_data:
+            for w, s in product(
+                self.dataprep_params["window_size"],
+                self.dataprep_params["sample_strategy"],
+            ):
+                self.input_dim = w + 1
+                model_selection = GridSearch(
+                    self.backend,
+                    self.store,
+                    self.estimator_gen_fn,
+                    search_space=self.params,
+                    num_epochs=epochs,
+                    evaluation_metric="loss",
+                    label_columns=[self.label_col],
+                    feature_columns=["features"],
+                    verbose=1,
+                )
+                prepped = prep_data_spark(
+                    self.dataset,
+                    w,
+                    1,
+                    s,
+                    time_col=self.time_col,
+                    label_col=self.label_col,
+                    id_col=self.id_col,
+                    value_col=self.value_col,
+                )
+
+                model = model_selection.fit(prepped)
+                print(model.get_history())
+                if model.get_history()["val_acc"][-1] > self.best_acc:
+                    LOGGER.info(f'Found new best model with window size {w}')
+                    self.best_acc = model.get_history()["val_acc"][-1]
+                    self.best_model = model.keras()
+                    self.best_w = w
+        
+
+        LOGGER.print('MLP Training complete:')
+        LOGGER.print(f'\tValidation accuracy: {self.best_acc}')
+        LOGGER.print(f'\tWindow size: {self.best_w}')
+
+        # task = delayed(self._train_one)
+
+        # Parallel(n_jobs=len(self.dataprep_params["window_size"]))(task(w) for w in self.dataprep_params["window_size"])
+
+    def predict(self, X):
+        """Predicts on given data."""
+        if self.best_model is None:
+            raise ValueError("Model not trained yet.")
+
+        return self.best_model.predict(X)
+
+
+
+class MLP(Model):
+
+    def __init__(
+        self,
+        params,
+        dataset,
+        spark,
+        cache_prepped_data: bool = False,
+        n_workers: int = 1,
+        time_col: str = "time",
+        label_col: str = "label",
+        id_col: str = "id",
+        value_col: str = "value",
+        store_type: str = 'local',
+        store_path: str = '/tmp/cerebro',
+    ):
+        """
+        Dataset must be in form of (id, timestamp, value, label)
+        """
+        super(MLP, self).__init__(
+            params,
+            dataset,
+            spark,
+            cache_prepped_data=cache_prepped_data,
+            num_workers=n_workers,
+            time_col=time_col,
+            label_col=label_col,
+            id_col=id_col,
+            value_col=value_col,
+            store_type=store_type,
+            store_path=store_path,
+        )
 
     def estimator_gen_fn(self, params):
         model = tf.keras.models.Sequential()
@@ -184,54 +280,64 @@ class MLP:
 
     #     return (model.get_history()['val_acc'][0], model.keras())
 
-    def train(self, epochs=10):
-        """Trains model on pre-loaded data."""
-        if not self.cache_prepped_data:
-            for w, s in product(
-                self.dataprep_params["window_size"],
-                self.dataprep_params["sample_strategy"],
-            ):
-                self.input_dim = w + 1
-                model_selection = GridSearch(
-                    self.backend,
-                    self.store,
-                    self.estimator_gen_fn,
-                    search_space=self.params,
-                    num_epochs=epochs,
-                    evaluation_metric="loss",
-                    label_columns=[self.label_col],
-                    feature_columns=["features"],
-                    verbose=1,
-                )
-                prepped = prep_data_spark(
-                    self.dataset,
-                    w,
-                    1,
-                    s,
-                    time_col=self.time_col,
-                    label_col=self.label_col,
-                    id_col=self.id_col,
-                    value_col=self.value_col,
-                ).repartition(self.n_workers)
 
-                model = model_selection.fit(prepped)
-                if model.get_history()['val_acc'][0] > self.best_acc:
-                    LOGGER.info(f'Found new best model with window size {w}')
+class LSTM(Model):
+    def __init__(
+        self,
+        params,
+        dataset,
+        spark,
+        cache_prepped_data: bool = False,
+        n_workers: int = 1,
+        time_col: str = "time",
+        label_col: str = "label",
+        id_col: str = "id",
+        value_col: str = "value",
+        store_type: str = 'local',
+        store_path: str = '/tmp/cerebro',
+    ):
+        """
+        Dataset must be in form of (id, timestamp, value, label)
+        """
+        super(LSTM, self).__init__(
+            params,
+            dataset,
+            spark,
+            cache_prepped_data=cache_prepped_data,
+            num_workers=n_workers,
+            time_col=time_col,
+            label_col=label_col,
+            id_col=id_col,
+            value_col=value_col,
+            store_type=store_type,
+            store_path=store_path,
+        )
 
-                    self.best_acc = model.get_history()['val_acc'][0]
-                    self.best_model = model.keras()
-                    self.best_w = w
+    def estimator_gen_fn(self, params):
+        model = tf.keras.models.Sequential()
+        input_dim = self.input_dim
+        model.add(tf.keras.layers.Input(shape=input_dim, name="features"))
+        model.add(tf.keras.layers.Reshape((1, -1)))
+        if params["num_hidden_layers"] > 1:
+            for i in range(params['num_hidden_layers']):
+                model.add(tf.keras.layers.LSTM(params['hidden_dim'], return_sequences=True, name=f'lstm{i}'))
+        model.add(
+            tf.keras.layers.LSTM(
+                params["hidden_dim"], activation=params["final_activation"]
+            )
+        )
 
-        LOGGER.print('MLP Training complete:')
-        LOGGER.print(f'\tValidation accuracy: {self.best_acc}')
-        LOGGER.print(f'\tWindow size: {self.best_w}')
+        optimizer = tf.keras.optimizers.SGD(
+            lr=params["lr"], momentum=params["momentum"]
+        )
+        loss = "mse"
 
-        # task = delayed(self._train_one)
-        # Parallel(n_jobs=len(self.dataprep_params["window_size"]))(task(w) for w in self.dataprep_params["window_size"])
+        estimator = SparkEstimator(
+            model=model,
+            optimizer=optimizer,
+            loss=loss,
+            metrics=["acc"],
+            batch_size=params["batch_size"],
+        )
 
-    def predict(self, X):
-        """Predicts on given data."""
-        if self.best_model is None:
-            raise ValueError("Model not trained yet.")
-
-        return self.best_model.predict(X)
+        return estimator
