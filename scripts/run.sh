021c666dca2158ecd39e11a29edff8fdcc298187
afzhou <afzhou@ucsd.edu>
2021-12-09 00:02:53 -0800

    Add scripts

---
+++ scripts/run.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+cd cse234 || exit
+
+# git diff -q && zip -r /tmp/cse234.zip cerebro_ts
+
+export PATH=/opt/py3.7-pyspark2.4-venv/bin/:"$PATH"
+export OUTPUT_FILE=/tmp/cerebro.log
+
+exec /opt/spark/bin/spark-submit \
+	--num-executors "${EXECUTORS:-4}" \
+	--master spark://test-project-master:7077 \
+	-c spark.executorEnv.PATH="$PATH" \
+	-c spark.executorEnv.TF_CPP_MIN_LOG_LEVEL=2 \
+	--py-files /tmp/cse234.zip \
+	driver.py \
+	--hdfs \
+	"$@"
---
+++ scripts/update.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd ~/cse234
+git pull
+zip -r /tmp/cse234.zip cerebro_ts
