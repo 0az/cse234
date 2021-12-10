cf9991db6ae68cf6d05d64f213292ec3f1415a44
afzhou <afzhou@ucsd.edu>
2021-12-09 16:41:53 -0800

    Fix cli

--- cerebro_ts/__main__.py
+++ cerebro_ts/__main__.py
@@ -3,26 +3,28 @@ import argparse
 from cerebro_ts.utils import positive_int
 
 ap = argparse.ArgumentParser()
-ap.add_argument(
-    '--workers',
-    '-w',
-    type=positive_int,
-    default=1,
-    help='Number of Cerebro worker instances',
-)
-ap.add_argument(
-    '--size',
-    '-s',
-    type=positive_int,
-    default=1,
-    help='Approximate dataset size in MB',
-)
-ap.add_argument(
-    '--data-path',
-    '-p',
-    required=True,
-    help='Dataset path',
-)
+
+def add_common_args(parser: argparse.ArgumentParser):
+    parser.add_argument(
+        '--workers',
+        '-w',
+        type=positive_int,
+        default=1,
+        help='Number of Cerebro worker instances',
+    )
+    parser.add_argument(
+        '--size',
+        '-s',
+        type=positive_int,
+        default=1,
+        help='Approximate dataset size in MB',
+    )
+    parser.add_argument(
+        '--data-path',
+        '-p',
+        required=True,
+        help='Dataset path',
+    )
 
 sp = ap.add_subparsers(title='Commands', dest='command')
 
@@ -35,6 +37,9 @@ gp = sp.add_parser(
     help='Generate a synthetic dataset',
 )
 
+add_common_args(xp)
+add_common_args(gp)
+
 xp.add_argument(
     '--grid-preset',
     '-g',
@@ -42,6 +47,7 @@ xp.add_argument(
     default='small',
     help='Hyperparameter grid preset',
 )
+
 store_type = xp.add_mutually_exclusive_group()
 store_type.add_argument(
     '--local',
@@ -58,11 +64,13 @@ store_type.add_argument(
     help='Use the HDFSStore Cerebro storage driver',
 )
 store_type.set_defaults(store_type='local')
+
 xp.add_argument(
     '--store-path',
     default='/tmp/cerebro',
     help='Location of the Cerebro data store',
 )
+
 args = ap.parse_args()
 
 
